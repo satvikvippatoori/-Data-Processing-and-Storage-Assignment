26c0f2349d3bb59496f90bc63af442b8c440b8b0
satvikvippatoori <115091002+satvikvippatoori@users.noreply.github.com>
2025-05-01 15:25:46 -0400

    changes

---
+++ in_memory_db.py
@@ -0,0 +1,38 @@
+class TransactionError(Exception):
+    pass
+
+class InMemoryDB:
+    def __init__(self):
+        self._main_store = {}  # committed data
+        self._transaction_store = None  # uncommitted data
+        self._in_transaction = False
+
+    def get(self, key):
+        if self._in_transaction and key in self._transaction_store:
+            return self._transaction_store[key]
+        return self._main_store.get(key)
+
+    def put(self, key, val):
+        if not self._in_transaction:
+            raise TransactionError("put() called outside of transaction")
+        self._transaction_store[key] = val
+
+    def begin_transaction(self):
+        if self._in_transaction:
+            raise TransactionError("Transaction already in progress")
+        self._transaction_store = {}
+        self._in_transaction = True
+
+    def commit(self):
+        if not self._in_transaction:
+            raise TransactionError("commit() called without an active transaction")
+        for key, value in self._transaction_store.items():
+            self._main_store[key] = value
+        self._transaction_store = None
+        self._in_transaction = False
+
+    def rollback(self):
+        if not self._in_transaction:
+            raise TransactionError("rollback() called without an active transaction")
+        self._transaction_store = None
+        self._in_transaction = False
---
+++ test_db.py
@@ -0,0 +1,34 @@
+from in_memory_db import InMemoryDB, TransactionError
+
+db = InMemoryDB()
+
+print(db.get("A"))  # None
+
+try:
+    db.put("A", 5)
+except TransactionError as e:
+    print(e)
+
+db.begin_transaction()
+db.put("A", 5)
+print(db.get("A"))  # Should be None
+
+db.put("A", 6)
+db.commit()
+print(db.get("A"))  # 6
+
+try:
+    db.commit()
+except TransactionError as e:
+    print(e)
+
+try:
+    db.rollback()
+except TransactionError as e:
+    print(e)
+
+print(db.get("B"))  # None
+db.begin_transaction()
+db.put("B", 10)
+db.rollback()
+print(db.get("B"))  # None
